c7586f19cd70ba8c3af571c585b85ba9f29a193b
Iulia <petcuiuliana220@gmail.com>
2026-02-15 22:28:57 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,8 @@ import Booking from './pages/Booking';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
+
+
 import UserDashboard from './pages/user/Dashboard';
 import UserBookings from './pages/user/Bookings';
 import UserProfile from './pages/user/Profile';
