0ad2542c9563183e0af6a18118669e6b2103cd6d
lenasavciuc06-pixel <lenasavciuc06@gmail.com>
2026-05-20 03:02:51 +0300

    feat: extend IUserAction with UpdateAvatar method

--- backend/Voyago.BusinessLayer/Interfaces/IUserAction.cs
+++ backend/Voyago.BusinessLayer/Interfaces/IUserAction.cs
@@ -1,11 +1,12 @@
 using Voyago.Domain.Dtos;
-
+ 
 namespace Voyago.BusinessLayer.Interfaces;
-
+ 
 public interface IUserAction
 {
     List<UserDto> GetAll();
     UserDto? GetById(int id);
     UserDto? Update(int id, UserDto dto);
     bool Delete(int id);
+    UserDto? UpdateAvatar(int id, string avatarUrl);  // <-- ADAUGAT
 }
