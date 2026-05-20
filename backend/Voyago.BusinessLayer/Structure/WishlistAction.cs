e33494b76dad4807090a74be376ff53ce69640d7
lenasavciuc06-pixel <lenasavciuc06@gmail.com>
2026-05-20 03:30:26 +0300

    feat: add WishlistAction concrete class

--- backend/Voyago.BusinessLayer/Structure/WishlistAction.cs
+++ backend/Voyago.BusinessLayer/Structure/WishlistAction.cs
@@ -1,6 +1,14 @@
+using Voyago.BusinessLayer.Core;
+using Voyago.BusinessLayer.Interfaces;
+using Voyago.Domain.Dtos;
+using Voyago.Domain.Entities;
+
 namespace Voyago.BusinessLayer.Structure;
 
-public class WishlistAction
+public class WishlistAction : WishlistActions, IWishlistAction
 {
-    
+    public List<Wishlist> GetByUserId(int userId) => ExecuteGetByUserId(userId);
+    public Wishlist? GetById(int id)              => ExecuteGetById(id);
+    public Wishlist Add(WishlistDto dto)           => ExecuteAdd(dto);
+    public bool Delete(int id)                    => ExecuteDelete(id);
 }
