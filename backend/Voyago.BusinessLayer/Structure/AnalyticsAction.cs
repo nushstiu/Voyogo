4b99d9492fb80fc9c6a369ea6065b456dc12699a
lenasavciuc06-pixel <lenasavciuc06@gmail.com>
2026-05-26 17:18:50 +0300

    feat: add AnalyticsAction concrete class

--- backend/Voyago.BusinessLayer/Structure/AnalyticsAction.cs
+++ backend/Voyago.BusinessLayer/Structure/AnalyticsAction.cs
@@ -1,6 +1,13 @@
+using Voyago.BusinessLayer.Core;
+using Voyago.BusinessLayer.Interfaces;
+
 namespace Voyago.BusinessLayer.Structure;
 
-public class AnalyticsAction
+public class AnalyticsAction : AnalyticsActions, IAnalyticsAction
 {
-    
+    public object GetOverview()                      => ExecuteGetOverview();
+    public List<object> GetBookingTrends()           => ExecuteGetBookingTrends();
+    public List<object> GetPopularDestinations()     => ExecuteGetPopularDestinations();
+    public List<object> GetRevenueByDestination()    => ExecuteGetRevenueByDestination();
+    public List<object> GetBookingStatusDistribution() => ExecuteGetBookingStatusDistribution();
 }
